3a15d009cb74bf9322499f844807442ec9155958
Ganesh Yellapu <98867550+Ganeshyellapu@users.noreply.github.com>
2022-09-09 11:24:12 +0530

    import java.util.Scanner; class addition {     public static void main(String args[])     {         int a,b,sum;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();
            sum=a+b;
             
            System.out.print(sum);
            
        }
        
    }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class addition
+{
+    public static void main(String args[])
+    {
+        int a,b,sum;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        
+        sum=a+b;
+         
+        System.out.print(sum);
+        
+    }
+    
+}
