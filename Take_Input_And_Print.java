5ff410824637297732ca72e3259816e1e39ea79b
Ganesh Yellapu <98867550+Ganeshyellapu@users.noreply.github.com>
2022-09-09 20:24:19 +0530

    import java.util.Scanner; class read {     public static void main(String args[])     {         String a;         Scanner sc=new Scanner(System.in);         a=sc.nextLine();         System.out.print(a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class read
+{
+    public static void main(String args[])
+    {
+        String a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextLine();
+        System.out.print(a);
+    }
+}
