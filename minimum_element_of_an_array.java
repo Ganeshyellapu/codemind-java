14054e50cc8807cae10a555ecc25886ea522bbfc
Ganesh Yellapu <98867550+Ganeshyellapu@users.noreply.github.com>
2022-10-07 15:49:16 +0530

    import java.util.*; class Minimum {     public static void main(String args[])     {         int n,min;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int[] arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         min=arr[0];         for(int i=0;i<n;i++)         {             if(arr[i]<min)             {                 min=arr[i];             }         }         System.out.print(min);     } }

---
+++ minimum_element_of_an_array.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Minimum
+{
+    public static void main(String args[])
+    {
+        int n,min;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        min=arr[0];
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]<min)
+            {
+                min=arr[i];
+            }
+        }
+        System.out.print(min);
+    }
+}
