50dfec054e1c545ed80d2658378103b10f95430c
Ganesh Yellapu <98867550+Ganeshyellapu@users.noreply.github.com>
2022-10-07 23:06:35 +0530

    import java.util.*; class Elements_grt_avg {     public static void main(String args[])     {         int n,sum=0,avg;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int[] arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             sum=sum+arr[i];         }         avg=sum/n;         int c=0;         for(int i=0;i<n;i++)         {             if(arr[i]>=avg)             {                 c+=1;             }         }         System.out.print(c);     } }

---
+++ elements_greater_than_average.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Elements_grt_avg
+{
+    public static void main(String args[])
+    {
+        int n,sum=0,avg;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            sum=sum+arr[i];
+        }
+        avg=sum/n;
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>=avg)
+            {
+                c+=1;
+            }
+        }
+        System.out.print(c);
+    }
+}
