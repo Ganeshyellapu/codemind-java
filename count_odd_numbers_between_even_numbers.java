2dbd6e6597f72c7b2fe4d6c447492dd364c5cac4
Ganesh Yellapu <98867550+Ganeshyellapu@users.noreply.github.com>
2022-10-11 11:17:28 +0530

    import java.util.*; class Count {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int [] a=new int[n];         int c=0;         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(int i=1;i<n-1;i++)         {             if(a[i-1]%2==0 && a[i+1]%2==0)             {                 if(a[i]%2!=0)                 {                     c=c+1;                 }             }         }         System.out.print(c);     } }

--- count_odd_numbers_between_even_numbers.java
+++ count_odd_numbers_between_even_numbers.java
@@ -1,24 +1,23 @@
 import java.util.*;
-class Odd_count
+class Count
 {
     public static void main(String args[])
     {
-        int n;
         Scanner sc=new Scanner(System.in);
-        n=sc.nextInt();
-        int[] arr=new int[n];
-        for(int i=0;i<n;i++)
-        {
-            arr[i]=sc.nextInt();
-        }
+        int n=sc.nextInt();
+        int [] a=new int[n];
         int c=0;
         for(int i=0;i<n;i++)
         {
-            if(i-1>=0 && i+1<n)
+            a[i]=sc.nextInt();
+        }
+        for(int i=1;i<n-1;i++)
+        {
+            if(a[i-1]%2==0 && a[i+1]%2==0)
             {
-                if(arr[i-1]%2==0 && arr[i]%2!=0 && arr[i+1]%2==0)
+                if(a[i]%2!=0)
                 {
-                    c+=1;
+                    c=c+1;
                 }
             }
         }
