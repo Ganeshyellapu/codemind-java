2d55c1f1f6b25aada7a875b7abaa9f9db63321f8
Ganesh Yellapu <98867550+Ganeshyellapu@users.noreply.github.com>
2022-10-11 11:14:09 +0530

    import java.util.*; class Count {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int [] a=new int[n];         int c=0;         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(int i=1;i<n-1;i++)         {             if((a[i-1]%2==0 && a[i+1]%2!=0) || (a[i-1]%2!=0 && a[i+1]%2==0))             {                 c=c+1;             }         }         System.out.print(c);     } }

--- count_numbers_between_even_and_odd_numbers_or_vice_versa.java
+++ count_numbers_between_even_and_odd_numbers_or_vice_versa.java
@@ -1,26 +1,21 @@
 import java.util.*;
-class Viceversa
+class Count
 {
     public static void main(String args[])
     {
-        int n;
         Scanner sc=new Scanner(System.in);
-        n=sc.nextInt();
-        int[] arr=new int[n];
-        for(int i=0;i<n;i++)
-        {
-            arr[i]=sc.nextInt();
-        }
+        int n=sc.nextInt();
+        int [] a=new int[n];
         int c=0;
         for(int i=0;i<n;i++)
         {
-            if(i-1>=0 && i+1<n)
+            a[i]=sc.nextInt();
+        }
+        for(int i=1;i<n-1;i++)
+        {
+            if((a[i-1]%2==0 && a[i+1]%2!=0) || (a[i-1]%2!=0 && a[i+1]%2==0))
             {
-                if((arr[i-1]%2==0 && arr[i+1]%2!=0)||(arr[i-1]%2!=0 && arr[i+1]%2==0))
-                {
-                    c+=1;
-                }
-                //if(arr[i-1]%2!=0 && arr[i])
+                c=c+1;
             }
         }
         System.out.print(c);
