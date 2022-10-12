18f69a551e14a7ec652a56ffe11756fffbdc2fed
Ganesh Yellapu <98867550+Ganeshyellapu@users.noreply.github.com>
2022-10-12 22:52:43 +0530

    import java.util.*; class Element_count {     public static int count(int k,int[] a)     {         int ct=0;         for(int i=0;i<a.length;i++)         {             if(a[i]==k)             {                 ct+=1;             }         }         return ct;     }     public static void main(String args[])     {         int n;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int[] arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }
            int[] b=new int[n];
            int index=0;
            for(int i=0;i<n;i++)
            {
                int flag=0;
                for(int j=0;j<i;j++)
                {
                    if(arr[i]==arr[j])
                    {
                        flag=1;
                        break;
                    }
                }
                if(flag==0)
                {
                    b[index]=arr[i];
                    index++;
                }
            }
            int e=0;
            for(int i=0;i<index;i++)
            {
                if(b[i]==count(b[i],arr))
                {
                    e+=1;
                    System.out.print(b[i]+" ");
                }
            }
            if(e==0)
            {
                System.out.print(-1);
            }
        }
    }

---
+++ Elements_same_as_count.java
@@ -0,0 +1,60 @@
+import java.util.*;
+class Element_count
+{
+    public static int count(int k,int[] a)
+    {
+        int ct=0;
+        for(int i=0;i<a.length;i++)
+        {
+            if(a[i]==k)
+            {
+                ct+=1;
+            }
+        }
+        return ct;
+    }
+    public static void main(String args[])
+    {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        
+        int[] b=new int[n];
+        int index=0;
+        for(int i=0;i<n;i++)
+        {
+            int flag=0;
+            for(int j=0;j<i;j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0)
+            {
+                b[index]=arr[i];
+                index++;
+            }
+        }
+        int e=0;
+        for(int i=0;i<index;i++)
+        {
+            if(b[i]==count(b[i],arr))
+            {
+                e+=1;
+                System.out.print(b[i]+" ");
+            }
+        }
+        if(e==0)
+        {
+            System.out.print(-1);
+        }
+    }
+}
