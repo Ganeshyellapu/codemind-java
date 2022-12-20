de9e15ea4e1865ce4b8ed7294634afda030319c3
Ganesh Yellapu <98867550+Ganeshyellapu@users.noreply.github.com>
2022-12-20 23:48:18 +0530

    import java.util.*; class Diamond {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int rows=sc.nextInt();         if(rows<=2)         {             System.out.println("The pattern is not possible");         }         else         {             for(int i=1;i<=rows;i++)             {                 for(int j=1;j<=i;j++)                 {                     System.out.print("*");                 }                 System.out.println();             }             for(int i=rows-1;i>=1;i--)             {                 for(int j=1;j<=i;j++)                 {                     System.out.print("*");                 }                 System.out.println();             }         }     } }

---
+++ Half_diamond_star_pattern.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Diamond
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int rows=sc.nextInt();
+        if(rows<=2)
+        {
+            System.out.println("The pattern is not possible");
+        }
+        else
+        {
+            for(int i=1;i<=rows;i++)
+            {
+                for(int j=1;j<=i;j++)
+                {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+            for(int i=rows-1;i>=1;i--)
+            {
+                for(int j=1;j<=i;j++)
+                {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
