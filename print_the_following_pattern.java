aa814a5019fa38d8628b18a3c5632da76338ddfc
Ganesh Yellapu <98867550+Ganeshyellapu@users.noreply.github.com>
2022-12-21 02:06:34 +0530

    import java.util.*; class patterns {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int rows=sc.nextInt();         for(int i=rows;i>=1;i--)         {             char c=(char)(i+64);             for(int j=1;j<=i;j++)             {                 System.out.print(c+" ");             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,18 +1,16 @@
 import java.util.*;
-class pattern
+class patterns
 {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
         int rows=sc.nextInt();
-        for(int i=1;i<=rows;i++)
+        for(int i=rows;i>=1;i--)
         {
+            char c=(char)(i+64);
             for(int j=1;j<=i;j++)
             {
-                //if(j==1||j==i||i==rows)
-                {
-                    System.out.print(j);
-                }
+                System.out.print(c+" ");
             }
             System.out.println();
         }
